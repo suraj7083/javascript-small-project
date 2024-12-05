3d93adc4dbbf71365e83de1c0a30af8caaec87f9
suraj7083 <157855829+suraj7083@users.noreply.github.com>
2024-12-05 16:39:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function bulboff() {
+    let i=document.getElementById('img')
+    i.src="off.jpg";
+    }
+
+    function bulbon() {
+        let i=document.getElementById('img')
+        i.src="on.jpg";
+        }
+    
